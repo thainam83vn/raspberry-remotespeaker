8dd70e2808ef11f7e3ff31efb4e8b3206770d466
Thai <thainam.tran@bammobi.com>
2019-04-29 22:19:49 -0400

    update port

--- server/index.js
+++ server/index.js
@@ -6,4 +6,4 @@ app.use(bodyparser());
 
 app.use('/api', require('./routes')());
 
-app.listen(80, () => console.log('server listen on 80'));
+app.listen(9090, () => console.log('server listen on 9090'));
