abcbec18b0c4eb41391cab6260c683a0bb78d33c
Thai <thainam.tran@bammobi.com>
2019-04-29 22:30:43 -0400

    update

--- server/services/cmd.js
+++ server/services/cmd.js
@@ -6,8 +6,10 @@ module.exports = () => {
   return {
     play: url => {
       current = exec('mpg321 /home/pi/song-1.mp3 -a bluealsa');
+      console.log('current:', current);
     },
     stop: () => {
+      console.log('stop current:', current);
       current.emit('exit', 1);
     }
   };
