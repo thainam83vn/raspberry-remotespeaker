0ab550799d3ae7cf0163709b7628ac4de61e9e4b
Thai <thainam.tran@bammobi.com>
2019-04-30 20:52:47 -0400

    add mpg321

--- server/services/cmd.js
+++ server/services/cmd.js
@@ -1,16 +1,20 @@
 var sys = require('sys');
 var exec = require('child_process').exec;
+var mpg321 = require('mpg321');
+const player = mpg321({ audiodevice: 'bluealsa' }).remote();
 
 module.exports = () => {
   let current;
   return {
     play: url => {
-      current = exec('mpg321 /home/pi/song-1.mp3 -a bluealsa');
+      // current = exec('mpg321 /home/pi/song-1.mp3 -a bluealsa');
+      player.play('/home/pi/song-1.mp3');
       console.log('current:', current);
     },
     stop: () => {
-      console.log(`kill ${current.pid}`);
-      exec(`kill ${current.pid}`);
+      console.log(`stop playing`);
+      player.stop();
+      // exec(`kill ${current.pid}`);
     }
   };
 };
