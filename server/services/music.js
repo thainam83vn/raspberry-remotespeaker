68fb99b375bee18e292dcc387f68f53f1387550c
Thai <thainam.tran@bammobi.com>
2019-05-03 19:33:30 -0400

    update

--- server/services/music.js
+++ server/services/music.js
@@ -4,13 +4,14 @@ var mpg321 = require('mpg321');
 const player = mpg321()
   .audiodevice('bluealsa')
   .remote();
+const MUSIC_FOLDER = '/home/pi/musics';
 
 module.exports = () => {
   let current;
   return {
     play: url => {
       // current = exec('mpg321 /home/pi/song-1.mp3 -a bluealsa');
-      player.play('/home/pi/*.mp3');
+      player.play(`${MUSIC_FOLDER}/${req.body.song}.mp3`);
       console.log('current:', current);
     },
     stop: () => {
