c8e334fe026724c16ed2e6c110d1923aa96e1437
Thai <thainam.tran@bammobi.com>
2019-05-16 19:22:24 -0400

    update vid

--- server/services/youtube.js
+++ server/services/youtube.js
@@ -1,12 +1,15 @@
 var sys = require('sys');
 var exec = require('child_process').exec;
+var axios = require('axios');
 
 module.exports = () => {
   return {
-    play: vid => {
-      console.log('Playing Youtube vid=' + vid);
-      // vid = vid || 'WwOY1o16T4s';
-      vid = 'aeHJHjkwDuM';
+    play: async vid => {
+      let songs = (await axios(
+        'https://s3.amazonaws.com/thainamtran-alexa/youtube_songs.txt'
+      )).data.split('\n');
+      songs = songs.map(s => s.replace('\r', ''));
+      vid = songs[0];
       exec('killall omxplayer.bin');
       exec(
         `omxplayer -o hdmi "\`youtube-dl -g -f mp4 https://youtube.com?v=${vid}\`"`
