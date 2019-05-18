4c5e0e03f476abe378ff0dd4ebf41637d2676131
Thai <thainam.tran@bammobi.com>
2019-05-18 15:30:00 -0400

    u

--- client/build/precache-manifest.893eff19ec705bbe8bd09d0709e45a18.js
+++ client/build/precache-manifest.893eff19ec705bbe8bd09d0709e45a18.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "94b64f894c196460e075eb4ab778fb9e",
+    "revision": "dc8f427b49a105f409ca7abf6e615a0e",
     "url": "/index.html"
   },
   {
-    "revision": "6340de87a3730044f4ae",
+    "revision": "2a4699261044ec305813",
     "url": "/static/css/main.2cce8147.chunk.css"
   },
   {
-    "revision": "c1e70ee55946819e3a7e",
-    "url": "/static/js/2.10b06523.chunk.js"
+    "revision": "87e913273ec8e6e3b27a",
+    "url": "/static/js/2.fa9dac84.chunk.js"
   },
   {
-    "revision": "6340de87a3730044f4ae",
-    "url": "/static/js/main.def1c9bf.chunk.js"
+    "revision": "2a4699261044ec305813",
+    "url": "/static/js/main.e501afbe.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
